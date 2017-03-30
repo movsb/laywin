be37274c3b7fad2796779bbc43e401e7da806e59
movsb <anhbk@qq.com>
2017-03-31 01:13:57 +0800

    header events update

--- ctrl/header.cpp
+++ ctrl/header.cpp
@@ -16,6 +16,24 @@ bool HeaderControl::filter_notify(int code, NMHDR* hdr, LRESULT* lr)
             return true;
         }
     }
+    else if(code == HDN_ENDTRACK){
+        if(on_end_track) {
+            *lr = on_end_track(hdr);
+            return true;
+        }
+    }
+    else if(code == HDN_DIVIDERDBLCLICK) {
+        if(on_divider_dblclick) {
+            *lr = on_divider_dblclick(hdr);
+            return true;
+        }
+    }
+    else if(code == HDN_ENDDRAG) {
+        if(on_end_drag) {
+            *lr = on_end_drag();
+            return true;
+        }
+    }
 
     return false;
 }
